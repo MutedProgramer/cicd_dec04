f79e52b02dd42f6c99b23a6c596e0b090fbd079e
MutedProgrammer <ethan.huy@gmail.com>
2025-12-11 17:33:14 -0600

    100% coverage

--- src/app.py
+++ src/app.py
@@ -8,13 +8,13 @@ def multiply (a,b):
     return a*b
 def division (a,b):
     if b == 0:
-        return ValueError("Cannot divide by zero")
+        raise ValueError("Cannot divide by zero")
     return a / b
 #advance operations
 
 def log (a, b=10):
     if a <= 0:
-        return ValueError("A must be greater than 0")
+        raise ValueError("A must be greater than 0")
     return math.log(a, b)
 
 def square(a):
